429e7b435ed5f10e02492fa9461327a5d7fef0a1
joshcaplin <joshcaplin@users.noreply.github.com>
2020-11-01 14:11:40 -0700

    Adding drag & drop functionality

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,9 +5,20 @@ import { Wasp } from '../../src'
 
 export default class Demo extends Component {
   render() {
+    let sampleList = [
+      '1 - aaa',
+      '2 - bbb',
+      '3 - ccc',
+      '4 - ddd',
+      '5 - eee',
+      '6 - fff',
+    ];
+
     return <div>
-      <h1>wasp Demo</h1>
-      <Wasp/>
+      <h1>WASP drag &amp; drop demo:</h1>
+      <Wasp
+        itemsToList={sampleList}
+      />
     </div>
   }
 }
